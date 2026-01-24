5d7218a7e1ff9fc7a487f0e62ddc77447ad398f9
astroabsa <64161946+astroabsa@users.noreply.github.com>
2026-01-24 19:55:53 +0530

    Update expiry.py

--- expiry.py
+++ expiry.py
@@ -1,14 +1,15 @@
 from datetime import datetime, timedelta
 
-def get_next_weekly_expiry():
+def get_next_nifty_expiry():
     today = datetime.now()
     weekday = today.weekday()  # Mon=0 ... Sun=6
 
-    days_to_thursday = (3 - weekday) % 7
-    expiry = today + timedelta(days=days_to_thursday)
+    # Tuesday = 1
+    days_to_tuesday = (1 - weekday) % 7
+    expiry = today + timedelta(days=days_to_tuesday)
 
-    # If today is Thursday and after 3:30 PM → take next week
-    if weekday == 3 and today.hour >= 15:
+    # If today is Tuesday after 3:30 PM → next week's expiry
+    if weekday == 1 and today.hour >= 15:
         expiry += timedelta(days=7)
 
     return expiry.strftime("%d-%b-%Y")
